8df34532832b36c3eae5eb787b5846987a996d48
liangweiting <76460001+weiting-liang@users.noreply.github.com>
2025-01-20 17:17:02 +0800

    Create gtdbtk.sh

---
+++ Genome_Annotation/gtdbtk.sh
@@ -0,0 +1,1 @@
+export PATH=/path/anaconda3/envs/gtdbtk-2.4.0/bin:$PATH && export GTDBTK_DATA_PATH=/path/anaconda3/envs/gtdbtk-2.4.0/share/gtdbtk-2.4.0/db  && gtdbtk classify_wf --genome_dir /path/SGB --out_dir ./output --extension fa --skip_ani_screen --cpus 8 --pplacer_cpus 1 > ./log.txt
